d2219d5a1ec8dbde5bc5fe65c0d1aa93154d79b0
Jennifer Zilli <jenniferz@rpc.com.br>
2022-08-31 11:21:43 -0300

    estilizando comentario

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,29 +1,38 @@
-import styles from './Comment.module.css';
+import { ThumbsUp } from "phosphor-react";
+import { Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
 
-export function Comment(){
-    return (
-        <div className={styles.comment}>
-            <img src='src="https://avatars.githubusercontent.com/u/50343150?v=4' />
-            <div className={styles.commentBox}>
-
-                <div className={styles.commentContent}>
-
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>Jennifer</strong>
-                            <time title="22 de Agosto ás 10:30h" dateTime="2022-08-22 10:30:00">Cerca de 1 hora atrás</time>
-
-                        </div>
-                    </header>
-
-                    <p>Muito Bom!!!</p>
-                </div>
-
-                <footer>
-                    Aplaudir
-                </footer>
-
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src='https://avatars.githubusercontent.com/u/50343150?v=4' />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Jennifer</strong>
+              <time
+                title="22 de Agosto ás 10:30h"
+                dateTime="2022-08-22 10:30:00"
+              >
+                Cerca de 1 hora atrás
+              </time>
             </div>
+            <button title="deletar comentario">
+              <Trash size={20}></Trash>
+            </button>
+          </header>
+
+          <p>Muito Bom!!!</p>
         </div>
-    )
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
